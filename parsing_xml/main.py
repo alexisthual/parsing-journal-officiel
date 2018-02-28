744f45650473f198f80f8f3786894d37b751d332
Alexis Thual <alexisthual@gmail.com>
2018-02-28 13:15:44 +0100

    Refactor STRUCTURE_TXT to JSON format for summaries; Add SummaryTester for testing SummaryParser

--- parsing_xml/main.py
+++ parsing_xml/main.py
@@ -9,7 +9,26 @@ from databaseManager import DatabaseManager
 
 # %% Test cell
 
+
 # %% Main cell
+def recursiveSearch(dirAbsPath):
+    '''Util function iterating through the directories of given directory.
+    It returns False in case there are several possible directories.'''
+
+    directories = [os.path.join(dirAbsPath, d) for d in os.listdir(dirAbsPath) if os.path.isdir(os.path.join(dirAbsPath, d))]
+    if len(directories) == 0:
+        return True
+    elif len(directories) == 1:
+        return recursiveSearch(os.path.join(dirAbsPath, directories[0]))
+    else:
+        return False
+
+def shouldExploreDir(dirAbsPath):
+    '''Determines whether the given directory respects the format one expects
+    from a JORFSIMPLE archive.'''
+
+    return recursiveSearch(dirAbsPath)
+
 if __name__ == '__main__':
     # Generate regex for catching all XML files
     currentDir = os.getcwd()
@@ -29,12 +48,19 @@ if __name__ == '__main__':
     summaryParser = SummaryParser()
     articleParser = ArticleParser()
 
-    for filePath in tqdm(glob.glob(fileAbsPath, recursive=True)):
-        fileName = fileNameRegex.search(filePath).group(1)
+    # for filePath in tqdm(glob.glob(fileAbsPath, recursive=True)):
+    dataDirPath = os.path.join(currentDir, dataDir)
+    for dirName in tqdm(os.listdir(dataDirPath)):
+        dirAbsPath = os.path.join(dataDirPath, dirName, 'jorf/simple/JORF')
 
-        if re.match('.*CONT.*', fileName) != None:
-            parsedSummary = summaryParser.parse(filePath)
-            dbm.addSummary(parsedSummary)
-        elif re.match('.*TEXT.*', fileName) != None:
-            parsedArticle = articleParser.parse(filePath)
-            dbm.addArticle(parsedArticle)
+        if os.path.isdir(dirAbsPath) and shouldExploreDir(dirAbsPath):
+            dirAbsPathRegex = os.path.join(dirAbsPath, 'CONT/**/*.xml')
+            for filePath in tqdm(glob.glob(dirAbsPathRegex, recursive=True)):
+                fileName = fileNameRegex.search(filePath).group(1)
+
+                if re.match('.*CONT.*', fileName) != None:
+                    parsedSummary = summaryParser.parse(filePath)
+                    dbm.addSummary(parsedSummary)
+                elif re.match('.*TEXT.*', fileName) != None:
+                    parsedArticle = articleParser.parse(filePath)
+                    dbm.addArticle(parsedArticle)
