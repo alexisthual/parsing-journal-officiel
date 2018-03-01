7ceac555c4c9ac0bba70ae4d69d9c34e3ef04dd1
Alexis Thual <alexisthual@gmail.com>
2018-03-01 14:49:57 +0100

    More generic ftpClient

--- parsing_xml/ftpClient.py
+++ parsing_xml/ftpClient.py
@@ -5,17 +5,16 @@ from tqdm import tqdm
 
 
 class FTPClient:
-    def __init__(self, outputFolder, verbose=False):
+    def __init__(self, verbose=False):
         '''Inits connexion with the distant FTP server.'''
 
         # ping echanges.dila.gouv.fr
         # 185.24.187.136
         self.ftp = FTP('echanges.dila.gouv.fr')
         self.ftp.login()
-        self.outputFolder = outputFolder
         self.verbose = verbose
 
-    def retrieveFiles(self, dirPath):
+    def retrieveFiles(self, dirPath, outputFolder, regex=None):
         '''Downloads all files in a given directory.'''
 
         self.ftp.cwd(dirPath)
@@ -24,14 +23,15 @@ class FTPClient:
         if self.verbose:
             print('Retrieved file list.')
 
-        fileNames = list(filter(lambda x: re.match('.*2018.*\-.*\.tar\.gz', x), fileNames))
+        if regex:
+            fileNames = list(filter(lambda x: re.match(regex, x), fileNames))
 
         if self.verbose:
             print('Downloading tarballs...')
 
         for fileName in tqdm(fileNames):
             if not re.match('.*Freemium.*', fileName):
-                with open(os.path.join(self.outputFolder, fileName), 'wb') as f:
+                with open(os.path.join(outputFolder, fileName), 'wb') as f:
                     self.ftp.retrbinary('RETR {}'.format(fileName), f.write)
                     f.close()
 
@@ -40,7 +40,10 @@ class FTPClient:
 
 
 # %% Test Cell
-outputFolder = '/home/alexis/parsing-journal-officiel/parsing_xml/data/JORFSIMPLE/tarballs/'
-ftpClient = FTPClient(outputFolder, verbose=True)
-ftpClient.retrieveFiles('JORFSIMPLE')
+ftpClient = FTPClient(verbose=True)
+ftpClient.retrieveFiles(
+    'JORFSIMPLE',
+    '/home/alexis/parsing-journal-officiel/parsing_xml/data/JORFSIMPLE/tarballs/',
+    regex='.*2018.*\-.*\.tar\.gz'
+)
 ftpClient.terminate()
