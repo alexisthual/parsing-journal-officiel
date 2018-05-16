d280b646f4755171a6e69a1e454f9b3415672f01
Alexis Thual <alexisthual@gmail.com>
2018-05-16 17:18:21 +0200

    Add customizable regexes for xml exploration

--- utils/exploreXMLStructure.py
+++ utils/exploreXMLStructure.py
@@ -16,14 +16,14 @@ def recursiveDefaultDict():
 
 
 class tarballExplorer():
-    def __init__(self, tarballAbsPath, outputFilePath):
+    def __init__(self, tarballAbsPath, outputFilePath, fileNameRegex, verbose=False):
         self.tarballAbsPath = tarballAbsPath
         self.outputFilePath = outputFilePath
         self.structure = defaultdict(recursiveDefaultDict)
+        self.verbose = verbose
 
         self.stopTags = ['table', 'br']
-        self.fileNameRegex =\
-            re.compile('.*jorf/simple/JORF/CONT/([0-9]{2}/){5}[a-zA-Z0-9]+/[a-zA-Z0-9]+\.xml')
+        self.fileNameRegex = fileNameRegex
 
     def updateStructure(self, structure, xmlElement):
         structure[xmlElement.tag]['attributes'] = []
@@ -37,18 +37,26 @@ class tarballExplorer():
     def explore(self):
         # Do first pass on data in order to count
         # total number of files
+        if self.verbose:
+            print('Counting selectable files in tarball...')
+
         totalNumberMembers = 0
         with tarfile.open(self.tarballAbsPath, 'r|gz') as tar:
-            member = tar.next()
-            while member:
-                if re.match(self.fileNameRegex, member.name):
-                    totalNumberMembers += 1
+            with tqdm(disable=(not self.verbose)) as pbar:
                 member = tar.next()
+                while member:
+                    if re.match(self.fileNameRegex, member.name):
+                        totalNumberMembers += 1
+                    member = tar.next()
+                    pbar.update(1)
 
         # Iterate through files in the tarball and update structure
         # for each file
+        if self.verbose:
+            print('Iterating through tarball\'s files...')
+
         with tarfile.open(self.tarballAbsPath, 'r|gz') as tar:
-            with tqdm(total=totalNumberMembers) as pbar:
+            with tqdm(total=totalNumberMembers, disable=(not self.verbose)) as pbar:
                 member = tar.next()
 
                 while member:
@@ -77,16 +85,28 @@ def setValueNestedDict(nestedDicts, keyList, value):
     getNestedDictKey(nestedDicts, keyList[:-1])[keyList[:-1]] = value
 
 if __name__ == '__main__':
+    # CONSTANTS
+    regexes = {
+        'jo_article': '.*jorf/simple/JORF/CONT/([0-9]{2}/){5}[a-zA-Z0-9]+/[a-zA-Z0-9]+\.xml',
+        'legi_text': '.*LEGITEXT.*\.xml',
+        'legi_arti': '.*LEGIARTI.*\.xml'
+    }
+
     # Read CLI arguments
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('configPath', metavar='configPath',
-            help='Path to YML config file')
+        help='Path to YML config file')
+    parser.add_argument('-r', dest='regexKey',
+        help='Key to select the regex which will be used to select files\n{}'.format(
+            '\n'.join(['\t{}: {}'.format(key, regexes[key]) for key in regexes])
+        )
+    )
     parser.add_argument('-i', dest='inputFilePath',
-            help='Path to input tarball')
+        help='Path to input tarball')
     parser.add_argument('-o', dest='outputFileName',
-            help='Output file name')
+        help='Output file name')
     parser.add_argument('-v', '--verbose', action='store_true',
-            help='Should print information and debug messages')
+        help='Should print information and debug messages')
     args = parser.parse_args()
 
     verbose = args.verbose
@@ -97,13 +117,16 @@ if __name__ == '__main__':
     if not os.path.exists(params['xmlStructuresDirPath']):
         os.makedirs(params['xmlStructuresDirPath'])
 
-    # CONSTANTS
+    # VARIABLES
+    regexFileName = re.compile(regexes[args.regexKey])
     tarballAbsPath = args.inputFilePath
     outputFilePath = os.path.join(
         params['xmlStructuresDirPath'],
         args.outputFileName
     )
 
-    explorer = tarballExplorer(tarballAbsPath, outputFilePath)
+    # Create tarballExplorer and launch main methods
+    explorer = tarballExplorer(tarballAbsPath, outputFilePath,
+                               regexFileName, verbose=verbose)
     explorer.explore()
     explorer.write()
