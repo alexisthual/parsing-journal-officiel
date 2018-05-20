851185ce712256e4aaf13b5bbc6d6cbfea126a28
Alexis Thual <alexisthual@gmail.com>
2018-05-20 19:59:46 +0200

    Add documentation to cleaning script; add counter for recursive objects

--- utils/cleanXMLStructure.py
+++ utils/cleanXMLStructure.py
@@ -1,7 +1,21 @@
 import argparse
 import json
+import operator
+
+from collections import Counter
+from functools import reduce
 
 if __name__ == '__main__':
+    '''
+    Simplifies the json structure extracted by ./exploreXMLStructure.py
+    In particular, it gets rid of useless XML tags and indicates recursion
+    of tags if need be.
+
+    Inputs:
+        * path to input json file
+        * path to output json file
+    '''
+
     # Read CLI arguments
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('-i', dest='inputFilePath',
@@ -11,19 +25,36 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # CONSTANTS
-    stopKeys = ['div', 'p', 'br', 'font', 'i', 'ol', 'li', 'b', 'attributes']
+    stopKeys = [
+        'div', 'p', 'br', 'font', 'i', 'ol', 'li', 'b',
+        'table', 'sup', 'sub', 'em', 'ul', 'hr',
+        'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'strong',
+        'attributes'
+    ]
 
-    def recDictCleaning(key, tree):
+    def recKeys(tree):
+        '''Returns the array of keys of all nested dicts of a given dict.'''
+
+        if isinstance(tree, dict):
+            return reduce(operator.add, tree.keys() + [recKeys(key) for key in tree])
+        else:
+            return []
+
+    def recDictCleaning(currentKey, tree):
+        '''Cleans a nested json dict from useless xml tags.'''
+
         if isinstance(tree, list):
             return tree
 
         rtree = dict()
-        for k in tree:
-            if (not k in stopKeys) or (k == 'attributes' and len(tree[k]) > 0):
-                if k == key:
-                    rtree['*REC'] = list(set(recDictCleaning(k, tree[k])))
+        for key in tree:
+            if (not key in stopKeys) or (key == 'attributes' and len(tree[key]) > 0):
+                # In case there seems to be a recursion,
+                # store all recursive keys and count them by type.
+                if key == currentKey:
+                    rtree['*REC'] = Counter(recKeys(recDictCleaning(key, tree[key])))
                 else:
-                    rtree[k] = recDictCleaning(k, tree[k])
+                    rtree[key] = recDictCleaning(key, tree[key])
 
         return rtree
 
